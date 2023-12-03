18d4eb12f822eed76f453ad98930e444589a5a84
jayednahian <jayednahian@yahoo.com>
2023-12-04 01:47:33 +0600

    card_and_selected done

---
+++ App/components/modalBuilder.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class ModalBuilder extends Component {
+  render() {
+    return (
+      <View>
+        <Text>modalBuilder</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
--- App/myApp.js
+++ App/myApp.js
@@ -1,10 +1,114 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import AllProduct from './2_functional_compoenet/PROJECT_1/product'
+import React, { Component } from 'react'
+import { FlatList, Text, TouchableOpacity, View } from 'react-native'
+import ModalBuilder from './components/modalBuilder'
+
 export default function MyApp() {
     return (
         <View>
-            <Text>hellow</Text>
+           <Test/>
+           <YourClassComponent/>
         </View>
   )
 }
+
+function Test(first) {
+    return(
+        <View style={{
+            flexDirection:'row',
+            justifyContent:"space-between",
+            paddingHorizontal:12,
+            paddingVertical:15,
+            borderWidth:1,
+            borderRadius:6,
+            borderColor:"#EBEBEB"
+            }}>
+            <View></View>
+            <View>
+                <Text style ={{}}>অচেনা পথিক</Text>
+                <Text>01838200551</Text>
+            </View>
+            <View>
+                <View>
+                <Text style={{
+                    backgroundColor:"#651FFF1A",
+                    paddingHorizontal:4,
+                    paddingVertical:2
+                }}>৩০ দিনের জন্য</Text>
+                </View>
+                
+                <Text>মেয়াদঃ ৩০/১২/২০২৩</Text>
+            </View>
+            <View></View>
+        </View>
+    )
+}
+
+
+
+
+
+class YourClassComponent extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        data: [
+          { "name": "jayed Khan", "id": 1 },
+          { "name": "jayed Khan", "id": 2 },
+          { "name": "jayed Khan", "id": 3 },
+          { "name": "jayed Khan", "id": 22 },
+            { "name": "jayed Khan", "id": 24 },
+            { "name": "jayed Khan", "id": 25 },
+            { "name": "jayed Khan", "id": 26 }
+        ],
+        selectedItems: [],
+      };
+    }
+  
+    renderItem = ({ item }) => (
+        <ListItem
+          item={item}
+          onPress={() => this.handleItemPress(item)}
+          isSelected={this.state.selectedItems.includes(item.id)}
+        />
+      );
+  
+    handleItemPress = (item) => {
+      let selectedItems = [...this.state.selectedItems];
+      if (selectedItems.includes(item.id)) {
+        selectedItems = selectedItems.filter(selectedId => selectedId !== item.id);
+      } else {
+        selectedItems.push(item.id);
+      }
+      this.setState({ selectedItems });
+    };
+  
+    render() {
+      return (
+        <FlatList
+          data={this.state.data}
+          renderItem={this.renderItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      );
+    }
+  }
+
+  const ListItem = ({ item, onPress, isSelected }) => (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        backgroundColor: isSelected ? 'lightblue' : 'white',
+        padding: 10,
+        marginBottom: 5,
+        borderRadius: 5,
+        borderWidth:1,
+        borderRadius:6,
+        borderColor:"#EBEBEB",
+      }}
+    >
+      <Text>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
+
