30c2a6e5f918ef4ef0a290941acc6a23a2742d18
jayednahian <jayednahian@yahoo.com>
2023-12-09 22:10:27 +0600

    2.4_ compnent style sheet seperated

---
+++ App/components/style.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native'
+
+
+const styles = StyleSheet.create({
+    font10:{
+        fontSize:10,
+        fontWeight:'400'
+    },
+    font14:{
+        fontSize:14,
+        fontWeight:'400'
+    },
+    font18:{
+        fontSize:18,
+        fontWeight:'400'
+    },
+    font22:{
+        fontSize:22,
+        fontWeight:'400'
+    },
+    paddingMargin10:{
+        padding:10,
+        margin:10
+    },
+    borderPaddingMargin10:{
+        borderWidth:2,
+        borderColor:'gray',
+        borderRadius:5,
+        padding:10,
+        margin:10
+    }
+    
+})
+
+
+export default styles;
+//App/components/style.js
