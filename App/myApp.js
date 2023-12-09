c1ff3269ed423490b640f49da0337369726e9a90
jayednahian <jayednahian@yahoo.com>
2023-12-09 22:09:06 +0600

    2.4_ update component

--- App/myApp.js
+++ App/myApp.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Counter from './components/counter'
 export default function MyApp() {
     return (
         <View>
             <Text>hellow</Text>
+			<Counter/>
         </View>
   )
 }
