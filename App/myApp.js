35f32f9d6b711329cf21a44162ce1bb3201ea2da
jayednahian <jayednahian@yahoo.com>
2024-02-10 12:46:16 +0600

    set_all_varaible_states

--- App/myApp.js
+++ App/myApp.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View } from 'react-native'
 import CounterHook from './components/counter'
 export default function MyApp() {
+    
+    let [length, setLength ] = useState(8);
+    let [checkNumber , setCheckNumber ] = useState(false);
+    let [checkCharacter, setCheckCharacter] = useState(false);
+    let [password, setPassword] = useState("")
+
     return (
         <View>
-            <Text>hellow</Text>
-			
+
         </View>
-  )
+    )
 }
