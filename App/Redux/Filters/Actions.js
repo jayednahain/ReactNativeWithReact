e8f83f33f7a51e19ca6d01f43fe5361a01949d7b
jayednahian <jayednahian@yahoo.com>
2024-02-13 22:43:15 +0600

    3.2: complete

---
+++ App/Redux/Filters/Actions.js
@@ -0,0 +1,19 @@
+import { COLORCHANGED, STATUSCHANGED } from "./ActionsTypes";
+
+export const colorChange = (color, changedType) => {
+    return {
+        typeof: COLORCHANGED,
+        payload: {
+            color, changedType
+        }
+    }
+}
+
+export const statusChange = (status) => {
+    return {
+        typeof: COLORCHANGED,
+        payload: {
+            status
+        }
+    }
+} 
