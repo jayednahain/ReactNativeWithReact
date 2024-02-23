8e3a7bc2d7c3cc725e3131fefd6a2b0ed63a66a7
jayednahian <jayednahian@yahoo.com>
2024-02-23 22:17:54 +0600

    3.6: on press readio button changes works successfully

--- App/Componetns/Todo.js
+++ App/Componetns/Todo.js
@@ -1,43 +1,56 @@
 import { StyleSheet, Text, Touchable, TouchableOpacity, View, Platform, UIManager, LayoutAnimation, Animated, Easing } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { fontStyle } from '../Style/Style'
 import Collapsible from 'react-native-collapsible';
 import { RadioButton } from 'react-native-paper';
+import  TodoActions  from '../Redux/Todos/Actions';
+import { useDispatch } from 'react-redux';
 
 
 const Todo = ({ todo }) => {
     let { text, id, completed } = todo;
+    const dispatched = useDispatch();
     let [isLargeText, setIsLargeText] = useState(false);
-    // let [storeLineNumber, setStoreLineNumber] = useState(0);
-    // let [lineNumber, setLineNumber] = useState(1);
     const [isCollapsed, setIsCollapsed] = useState(true);
-    const [checked, setChecked] = useState('first');
-
+    const [isChecked, setChecked] = useState(false);
+    const [isCompleted,setCompleted] = useState(false);
 
 
     const handleTextLayout = (event) => {
         const { lines } = event.nativeEvent;
         if (lines.length > 1) {
             setIsLargeText(true)
-            // setStoreLineNumber(lines.length)
         }
-        console.warn(lines.length)
     }
 
     const onPressItem = () => {
         setIsCollapsed(!isCollapsed);
     }
+    
+    useEffect(()=>{
+        setChecked(completed?true:false)
+        setCompleted(completed?true:false)
+    }, [completed])
+
+    const onPressRadioButton =(id)=>{
+        setChecked(!isChecked);
+        setCompleted(!isCompleted)
+        dispatched(TodoActions.toggled(id));
+    }
 
     return (
         <View style={{
             borderWidth: 1, marginBottom: 10, width: "100%",
-            borderRadius: 15, flexDirection: "row", paddingHorizontal: 10, paddingVertical: 15, alignItems: 'center'
+            borderRadius: 15, flexDirection: "row", 
+            paddingHorizontal: 10, 
+            paddingVertical: 8, alignItems: 'center'
         }}>
             <RadioButton
                 theme={""}
                 value="first"
-                status={checked === 'first' ? 'checked' : 'unchecked'}
-                onPress={() => setChecked('first')}
+                //status={checked === 'first' ? 'checked' : 'unchecked'}
+                status={isChecked ? 'checked'  : 'unchecked'}
+                onPress={()=>onPressRadioButton(id)}
             />
             <TouchableOpacity disabled={!isLargeText}
                 onPress={onPressItem}>
@@ -45,14 +58,16 @@ const Todo = ({ todo }) => {
                     {/* {isCollapsed ? */}
                     <Text
                         numberOfLines={1}
-                        style={[{ textDecorationLine: completed ? "line-through" : 'none' }, fontStyle.H4]}
+                        style={[{ 
+                            textDecorationLine:isChecked ? "line-through" : 'none'},
+                            fontStyle.H4]}
                         onTextLayout={handleTextLayout}>
                         {text}
                     </Text>
                     {/* :null} */}
                 </Collapsible>
                 <Collapsible collapsed={isCollapsed}>
-                    <Text style={[{}, fontStyle.H4]}>{text}</Text>
+                    <Text style={[{textDecorationLine:isCompleted ? "line-through" : 'none'}, fontStyle.H3]}>{text}</Text>
                 </Collapsible>
             </TouchableOpacity>
 
