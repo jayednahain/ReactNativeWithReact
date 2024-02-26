40e01d82aadd7352ba95d5b134b1d91df874d993
jayednahian <jayednahian@yahoo.com>
2024-02-26 21:14:48 +0600

    3.7:- remaning task working

--- App/Componetns/RemanningTaskShow.js
+++ App/Componetns/RemanningTaskShow.js
@@ -1,16 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { fontStyle } from '../Style/Style';
 
 export default function RemainingTaskShow() {
+
+    const todoList = useSelector((state) => state.todos)
+    const todoListNotCompleted = todoList.filter(todo => !todo.completed).length;
+
     return (
         <View style={{
-            backgroundColor:'red',
-            position:'absolute',
-            bottom:70,
-            right:0,
-            marginRight:20
-            }}>
-            <Text>RemainingTaskShow</Text>
+            backgroundColor:'#034883',
+            position: 'absolute',
+            bottom: 70,
+            right: 0,
+            marginRight: 20,
+            height:50,
+            width:100,
+            alignItems:'center',
+            justifyContent:'center',
+            // borderRadius:25
+            
+        }}>
+            <Text style={[{color:'white'},fontStyle.H3]}>Task left {todoListNotCompleted}</Text>
         </View>
     )
 }
