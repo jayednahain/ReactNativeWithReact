ae9cd71cb40b7bfc47c7da580993f73f6cd8af6f
jayednahian <jayednahian@yahoo.com>
2024-02-26 15:41:34 +0600

    3.7: started created more dummy data

--- App/Redux/Todos/InitialState.js
+++ App/Redux/Todos/InitialState.js
@@ -1,15 +1,85 @@
 const initialState = [
     {
         id: 1,
-        text: "Learn React JS Learn React JS Learn React JS earn React JS Learn React JS Learn React JS earn React JS Learn React JS Learn React JS ",
-        completed: true,
+        text: "Build a React Native app Build a React Native app Build a React Native app",
+        completed: false,
+      },
+    {
+      id: 2,
+      text: "Build a React Native app",
+      completed: false,
     },
     {
-        id: 2,
-        text: "Learn Redux",
-        completed: false,
-        color: "red",
+      id: 3,
+      text: "Learn Node.js",
+      completed: false,
+      color: "blue",
     },
-];
+    {
+      id: 4,
+      text: "Explore GraphQL",
+      completed: true,
+    },
+    {
+      id: 5,
+      text: "Write a blog post about Redux",
+      completed: false,
+      color: "green",
+    },
+    {
+        id: 6,
+        text: "Practice Databases",
+        completed: false,
+      },
+      {
+        id: 7,
+        text: "Attend React Meetup Attend React MeetupAttend React MeetupAttend React Meetup",
+        completed: true,
+      },
+      {
+        id: 8,
+        text: "Explore CSS-in-JS libraries",
+        completed: false,
+        color: "purple",
+      },
+      {
+        id: 9,
+        text: "Contribute to an open-source project",
+        completed: false,
+      },
+      {
+        id: 10,
+        text: "Read a book on Web Development",
+        completed: true,
+      },
+      {
+        id: 11,
+        text: "Learn about Progressive Web Apps",
+        completed: false,
+      },
+      {
+        id: 12,
+        text: "Experiment with TypeScript",
+        completed: true,
+        color: "orange",
+      },
+      {
+        id: 13,
+        text: "Participate in a coding challenge",
+        completed: false,
+      },
+      {
+        id: 14,
+        text: "Create a portfolio website",
+        completed: true,
+      },
+      {
+        id: 15,
+        text: "Master Git and GitHub workflows",
+        completed: false,
+        color: "brown",
+      },
+  ];
+  
 
 export default initialState;
