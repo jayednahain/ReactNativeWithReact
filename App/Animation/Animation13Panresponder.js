e4f538b5c1b1f6a952ef6c4ca2b47f806c3c2732
jayed.nahian <jayed.nahian@trucklagbe>
2024-05-02 16:35:27 +0600

    15 title complete :wokring with geture values

--- App/Animation/Animation13Panresponder.js
+++ App/Animation/Animation13Panresponder.js
@@ -1,10 +1,17 @@
-import { View, Text, Animated, PanResponder } from 'react-native'
+import { View, Text, Animated, PanResponder } from 'react-native';
+import { Dimensions } from 'react-native'
 import React, { useState } from 'react'
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default function Animation13PanresRonder() {
 
     // const pan = useState(new Animated.ValueXY())[0];
     const pan = useState(new Animated.ValueXY())[0];
+    // const width = useState(0)[0];
+    // const height = useState(0)[0];
 
 
 
@@ -20,23 +27,47 @@ export default function Animation13PanresRonder() {
             onPanResponderGrant: () => {
                 // if onMoveShouldSetPanResponder return true , onPanResponderGrant will run
                 console.log("### access confirm !")
-                pan.setOffset({
-                    x: pan.x._value,
-                    y: pan.y._value
-                })
+                // pan.setOffset({
+                //     x: pan.x._value,
+                //     y: pan.y._value
+                // })
             },
+
+            //move-1
             // onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
             //dx and dy delta value
             // relative distance of current position and new position
-
             // vx, vy are the velocity value
-            onPanResponderMove: (...args) => {
-                console.log("onPanResponderMove__ run")
+
+            //move - 2
+            onPanResponderMove: (_, gesture) => {
+                console.log("onPanResponderMove__ run: ",)
+                // pan.x.setValue(gesture.dx+2);
+                // pan.y.setValue(gesture.dy + 2);
+
+                console.log("windowHeight: ", windowHeight, " || ", gesture);
+                console.log("windowWidth", windowWidth)
+
+                if (gesture.moveX < windowWidth - 50 && 60 < gesture.moveX) {
+                    pan.x.setValue(gesture.dx);
+                }
+                if (gesture.moveY < windowHeight - 50 && 60 < gesture.moveY) {
+                    pan.y.setValue(gesture.dy);
+                }
+
+
+
             },
 
-            onPanResponderRelease: () => {
-                console.log("__onPanResponderRelease!")
-                pan.extractOffset();
+
+
+            onPanResponderRelease: (_, gesture) => {
+                // console.log("__onPanResponderRelease!: -- ", pan.x * 10)
+                // console.log("windowHeight", windowHeight);
+                // console.log("windowWidth",windowWidth)
+                // pan.x.setValue(gesture.dx);
+                // pan.y.setValue(gesture.dy);
+                // pan.extractOffset();
             },
         }),
     )[0];
@@ -53,12 +84,19 @@ export default function Animation13PanresRonder() {
                         height: 100,
                         width: 100,
                         borderRadius: 100 / 2,
-    
-                    }, pan.getLayout()]
-                    
-                    
-                    
-            }
+                        transform: [
+                            { translateX: pan.x },
+                            { translateY: pan.y }
+                        ]
+                        // top: pan.y,
+                        // left:pan.x
+                    },
+                        // pan.getLayout()
+                    ]
+
+
+
+                }
                 {...panResponder.panHandlers}
             />
         </View>
