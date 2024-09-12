e83cdcae55a0e45460f1e050284b37ba2c1af006
jayed.nahian <jayed.nahian@trucklagbe>
2024-09-12 17:12:53 +0600

    composite and pipe line

--- App/ReduxMosh.js
+++ App/ReduxMosh.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+var _ = require('lodash-contrib');  
+
 
 export default function ReduxMosh() {
+    const strTrip = str => str.trim();
+    const strView = str => <Text>{str}</Text>
+    const strUpperCase = str => str.toUpperCase();
+    const strTransform =  _.pipeline(strTrip,strUpperCase,strView)
+
+
     return (
         <View>
-            <Text>ReduxMosh</Text>
+            <Text>{strTransform("zakir  khan  ")}</Text>
         </View>
     )
 }
